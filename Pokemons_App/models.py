9268620799cee07877c80bbe012505c851a1519a
maayan <maayanmagal@gmail.com>
2022-01-09 22:40:07 +0200

    fin

--- Pokemons_App/models.py
+++ Pokemons_App/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator, MaxLengthValidator,MinLengthValidator
 
 # Create your models here.
 
 
 class Pokemon(models.Model):
-    Name = models.CharField(max_length=50, primary_key=True)
+    Name = models.CharField(validators=[MaxLengthValidator(50),MinLengthValidator(2)], primary_key=True)
     Type = models.CharField(max_length=50)
-    Generation = models.IntegerField()
+    Generation = models.IntegerField(validators=[MaxValueValidator(6),MinValueValidator(1)])
     Legendary = models.BooleanField()
-    Hp = models.IntegerField()
-    Attack=models.IntegerField()
-    Defense = models.IntegerField()
+    Hp = models.IntegerField(validators=[MaxValueValidator(300),MinValueValidator(1)])
+    Attack=models.IntegerField(validators=[MaxValueValidator(300),MinValueValidator(1)])
+    Defense = models.IntegerField(validators=[MaxValueValidator(300),MinValueValidator(1)])
